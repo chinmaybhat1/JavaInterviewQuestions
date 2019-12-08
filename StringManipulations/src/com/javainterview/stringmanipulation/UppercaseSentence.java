fe625db35bf005e787cd52158aa12d353d7e4058
Chinmay Bhat <chinmay.bhat@cerner.com>
2019-12-08 22:03:41 +0530

    Program to convert leading characters in a sentence to upper case

---
+++ StringManipulations/src/com/javainterview/stringmanipulation/UppercaseSentence.java
@@ -0,0 +1,34 @@
+package com.javainterview.stringmanipulation;
+
+/*
+ * Program to convert leading characters in a sentence to upper case
+ */
+public class UppercaseSentence {
+
+	public static void main(String[] args) {
+
+		String sentence = "this is a sentence with lower case words";
+
+		String uppercaseSentence = getUpperCaseSentence(sentence);
+
+		System.out.println("Upper case sentence: " + uppercaseSentence);
+	}
+
+	private static String getUpperCaseSentence(String sentence) {
+
+		StringBuffer uppercaseSentence = new StringBuffer();
+
+		String[] sentenceArray = sentence.split(" ");
+
+		if (sentenceArray.length > 0) {
+			for (int i = 0; i < sentenceArray.length; i++) {
+				String newString = sentenceArray[i].replace(String.valueOf(sentenceArray[i].charAt(0)),
+						String.valueOf(sentenceArray[i].charAt(0)).toUpperCase());
+				uppercaseSentence.append(newString + " ");
+			}
+		}
+
+		return uppercaseSentence.toString();
+	}
+
+}
